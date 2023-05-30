f1eb567ec9425ab795a93e2e49a6ecef57b12360
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-05-30 00:54:14 -0300

    feat:removeHours

--- src/main.js
+++ src/main.js
@@ -16,25 +16,8 @@ const chave_api = 'fd09144ec2677cbc9cfb5ee486269b3b';
 
 async function previsao(cidade) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave_api}&units=metric&lang=${lang}`;
-
     const get_data = await fetch(`${url}`).then(Response => Response.json());
-    console.log(get_data)
 
-    const time_zone = {timezone :`${get_data.timezone}`};
-    const horas = (-Math.sign(time_zone.timezone)) * Math.floor(Math.abs(time_zone.timezone) / 3600);
-    const op = horas >= 0 ? '+' : '';
-    const data = new Date();
-    const data_temp = data.toLocaleString('pt-BR', { timeZone: `Etc/GMT${op}${horas}`})
-    const hr_atual = new Array(data_temp.slice(12,14));
-    const min_atual = new Array(data_temp.slice(15,17));
-
-    // if (hr_atual[0] >= 0 || hr_atual[0] <= 11){
-    //     hours.textContent = `${hr_atual}:${min_atual} AM`;
-    // }else{
-    //     hours.textContent = `${hr_atual}:${min_atual} PM`;
-    // }
-
-    hours.textContent = `${data.getHours()}`
     if (get_data.cod == '404') {
         container_error.style.display = 'flex';
         container.style.display = 'none';
@@ -50,7 +33,7 @@ async function previsao(cidade) {
     }
 
     temperatura.innerHTML = `${get_data.main.temp.toFixed(0)}<sup>°C</sup>`;
-    
+
     if (get_data.weather[0].main == 'Rain') {
         box.style.backgroundImage = 'linear-gradient(#DCE9F2 25%, #A7CCFC)';
         input_cidade.style.backgroundColor = '#e2effb';
@@ -78,11 +61,11 @@ async function previsao(cidade) {
         img.src = 'assets/snow.png';
     }
 
-    local_cidade.textContent = `${get_data.name}` 
+    local_cidade.textContent = `${get_data.name}`
     clima_cidade.textContent = `${get_data.weather[0].description}`;
     humidade.textContent = `${get_data.main.humidity}%`;
     velocidade_vento.textContent = `${get_data.wind.speed * 3.701.toFixed()}Km/h`;
-}   
+}
 
 btn_procurar.addEventListener('click', () => {
     previsao(input_cidade.value);
